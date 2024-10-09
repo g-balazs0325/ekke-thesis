ea564c6d3e43dad919b177ef1232fc87269efc04
Gabriel Balazs <jobaba2003@gmail.com>
2024-10-09 09:26:46 +0200

    React-Three-Fiber library hozzáadása a projekthez

--- dev/src/app.tsx
+++ dev/src/app.tsx
@@ -1,8 +1,20 @@
+import { Canvas } from "@react-three/fiber";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 const MyApp = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Canvas>
+        <directionalLight position={[0, 1, 0]} />
+        <mesh>
+          <torusKnotGeometry />
+          <meshPhysicalMaterial />
+        </mesh>
+      </Canvas>
+    </>
+  );
 };
 
 root.render(<MyApp />);
