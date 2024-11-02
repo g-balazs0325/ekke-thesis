e7e1748ae033d17d06eee652e6adfee031a17e10
Gabriel Balazs <jobaba2003@gmail.com>
2024-11-02 16:25:38 +0100

    Alap canvas textúra alkalmazása

--- dev/src/app.tsx
+++ dev/src/app.tsx
@@ -1,20 +1,95 @@
 import { Canvas } from "@react-three/fiber";
+import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { CanvasTexture } from "three";
 
-const root = createRoot(document.getElementById("root"));
-const MyApp = () => {
+/*const MyApp = () => {
+  const myCanvasElement = React.useRef();
+  // ref stb. definíciók
+  // ref-et használó dolgok
+
+  React.useEffect(() => {
+    const myCanvas: HTMLCanvasElement = myCanvasElement.current;
+    const context = myCanvas.getContext("2d");
+    context.fillRect(0, 0, 256, 256);
+  }, []);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <canvas ref={"myCanvasElement"} width={256} height={256} hidden />
       <Canvas>
         <directionalLight position={[0, 1, 0]} />
         <mesh>
           <torusKnotGeometry />
-          <meshPhysicalMaterial />
+          <meshPhysicalMaterial>
+            <canvasTexture attach={"map"} image={myCanvasElement.current} />
+          </meshPhysicalMaterial>
         </mesh>
       </Canvas>
     </>
   );
-};
+};*/
 
+class MyApp extends React.Component {
+  private canvasRef: React.MutableRefObject<HTMLCanvasElement>;
+  private textureRef: React.MutableRefObject<CanvasTexture>;
+
+  constructor(props: any) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.textureRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const canvas: HTMLCanvasElement = this.canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "red";
+    context.fillRect(0, 0, 256, 256);
+
+    context.strokeStyle = "green";
+    context.lineWidth = 5;
+    context.lineCap = "round";
+
+    context.beginPath();
+    for (let i = 0; i < 10; i++) {
+      context.lineTo(Math.random() * 256, Math.random() * 256);
+    }
+    context.closePath();
+    context.stroke();
+  }
+
+  private initializeTexture(ref: CanvasTexture) {
+    const texture = ref;
+    const canvas: HTMLCanvasElement = this.canvasRef.current;
+
+    texture.image = canvas;
+    texture.needsUpdate = true;
+
+    this.textureRef.current = texture;
+  }
+
+  render() {
+    return (
+      <React.StrictMode>
+        <canvas ref={this.canvasRef} width={256} height={256} hidden />
+        <Canvas>
+          <directionalLight position={[0, 1, 0]} />
+          <ambientLight intensity={0.5} />
+          <mesh>
+            <torusKnotGeometry />
+            <meshPhysicalMaterial>
+              <canvasTexture
+                ref={(ref) => this.initializeTexture(ref)}
+                attach={"map"}
+              />
+            </meshPhysicalMaterial>
+          </mesh>
+        </Canvas>
+      </React.StrictMode>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById("root"));
 root.render(<MyApp />);
