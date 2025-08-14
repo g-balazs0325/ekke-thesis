85fc1b7a2d5ed71992b7c93dc4e6555812290564
Gabriel Balazs <jobaba2003@gmail.com>
2025-08-14 11:02:43 +0200

    Vector3 egyenlőségtesztelő írása

--- dev/src/test/vitest.setup.ts
+++ dev/src/test/vitest.setup.ts
@@ -1,14 +1,28 @@
 import { expect } from 'vitest'
-import { Vector2Like } from 'three'
+import { Vector2Like, Vector3Like } from 'three'
 
 function isVector2(a: unknown): a is Vector2Like {
   return typeof a === 'object' && 'x' in a && 'y' in a && !('z' in a)
 }
-function vector2Equals(a: unknown, b: unknown): boolean {
+function vector2Equals(a: unknown, b: unknown): boolean | undefined {
   const aIsVector2 = isVector2(a)
   const bIsVector2 = isVector2(b)
 
   if (aIsVector2 && bIsVector2) return a.x === b.x && a.y === b.y
+  else if (aIsVector2 === bIsVector2) return undefined
   else return false
 }
-expect.addEqualityTesters([vector2Equals])
+
+function isVector3(a: unknown): a is Vector3Like {
+  return typeof a === 'object' && 'x' in a && 'y' in a && 'z' in a
+}
+function vector3Equals(a: unknown, b: unknown): boolean | undefined {
+  const aIsVector3 = isVector3(a)
+  const bIsVector3 = isVector3(b)
+
+  if (aIsVector3 && bIsVector3) return a.x === b.x && a.y === b.y && a.z === b.z
+  else if (aIsVector3 === bIsVector3) return undefined
+  else return false
+}
+
+expect.addEqualityTesters([vector2Equals, vector3Equals])
