850dbda8c8dcd17955971308166916a69fb95553
Gabriel Balazs <jobaba2003@gmail.com>
2025-10-08 10:08:03 +0200

    Vitest böngésző mód beállítása

--- dev/vitest.config.ts
+++ dev/vitest.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    browser: {
+      provider: 'playwright',
+      instances: [{ browser: 'chromium' }],
+      viewport: {
+        width: 800,
+        height: 600
+      }
+    },
     setupFiles: './src/test/vitest.setup.ts'
   }
 })
