b298609ec95147de33a563332fdbd988d0c86e71
Gabriel Balazs <jobaba2003@gmail.com>
2026-03-24 11:39:16 +0100

    CircleSelectorGUI implementálása

--- dev/src/renderer/src/core/painting/selectors/gui/CircleSelectorGUI.ts
+++ dev/src/renderer/src/core/painting/selectors/gui/CircleSelectorGUI.ts
@@ -3,6 +3,7 @@ import SelectorGUI from './SelectorGUI'
 
 export default class CircleSelectorGUI extends SelectorGUI {
   private selector: CircleSelector
+  private circle: HTMLDivElement
 
   public constructor(selector: CircleSelector, overlayDiv: HTMLDivElement) {
     super(overlayDiv)
@@ -10,12 +11,35 @@ export default class CircleSelectorGUI extends SelectorGUI {
   }
 
   public onSelected(): void {
-    throw new Error('Method not implemented.')
+    this.createCircleElement()
   }
-  public onMouseMove(): void {
-    throw new Error('Method not implemented.')
+  public onMouseMove(mouseX: number, mouseY: number): void {
+    this.updateCircleDimensions(mouseX, mouseY)
   }
   public onUnselected(): void {
-    throw new Error('Method not implemented.')
+    this.circle.remove()
+  }
+
+  private createCircleElement(): void {
+    const circle = document.createElement('div')
+
+    const style = circle.style
+    style.position = 'relative'
+    style.transform = 'translate(-50%, -50%)'
+    style.borderRadius = '50%'
+    style.border = '1px solid white'
+
+    this.circle = circle
+    this.getOverlayDiv().appendChild(this.circle)
+  }
+
+  private updateCircleDimensions(mouseX: number, mouseY: number): void {
+    const radius = this.selector.getRadius()
+
+    const style = this.circle.style
+    style.left = `${mouseX}px`
+    style.top = `${mouseY}px`
+    style.width = `${radius * 2}px`
+    style.height = `${radius * 2}px`
   }
 }
