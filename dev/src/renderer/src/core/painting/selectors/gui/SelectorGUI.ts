9adbed1eddc8eb928f000f5829ca140e34563317
Gabriel Balazs <jobaba2003@gmail.com>
2026-03-24 11:38:53 +0100

    Argumentumok megadása az onMouseMove metódushoz

--- dev/src/renderer/src/core/painting/selectors/gui/SelectorGUI.ts
+++ dev/src/renderer/src/core/painting/selectors/gui/SelectorGUI.ts
@@ -10,6 +10,6 @@ export default abstract class SelectorGUI {
   }
 
   public abstract onSelected(): void // pl. elem létrehozása a circleselectorhoz, állítható property hozzáadása a guihoz
-  public abstract onMouseMove(): void // elem mozgatása az egérhez
+  public abstract onMouseMove(mouseX: number, mouseY: number): void // elem mozgatása az egérhez
   public abstract onUnselected(): void // elem és gui property törlése
 }
