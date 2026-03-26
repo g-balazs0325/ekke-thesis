2cf8d50f3babb3476b817017abcde01e2bd9da67
Gabriel Balazs <jobaba2003@gmail.com>
2026-03-26 11:40:53 +0100

    Archivált projekt törlése

--- dev/__OLD/src/assets.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.hdr" {
-  const value: string;
-  export default value;
-}
--- dev/__OLD/src/core/painting/selectors/raycalculations/CameraRayCalculation.ts
+++
@@ -1,5 +0,0 @@
-import { Camera, Line3, Vector2 } from "three";
-
-export default interface CameraRayCalculation<T extends Camera> {
-  calculateCameraRay(camera: T, normalizedPosition: Vector2): Line3;
-}
